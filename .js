5f6b1dd4a86c5b02e0d0a6e601cbb8de942d8543
Christian Ruiz <christian.ruiz0828@gmail.com>
2021-10-02 13:43:21 -0500

    POO-ejercicios

---
+++ .js
@@ -0,0 +1,11 @@
+const Charmander = new Pokemon("Charmander", "Fire", ["Charmeleon", "Charizar"]);
+
+const Squirtle = new Pokemon("Squirtle", "Water", ["Wartortle", "Blastoise"]);
+
+console.log(`${Charmander.#name} es de tipo ${Charmander.#type}`);
+console.log(Charmander.attack());
+console.log(Charmander.evolve(0));
+
+console.log(`${Squirtle.#name} es de tipo ${Squirtle.#type}`);
+console.log(Squirtle.attack());
+console.log(Squirtle.evolve(0));
